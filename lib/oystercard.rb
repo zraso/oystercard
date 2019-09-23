52c8f523678f00ac1cd2b8c1ed5ebb868f056f4d
zraso <zaira.rasool@hotmail.co.uk>
2019-09-23 17:59:44 +0100

    Adds max balance guard clause

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,13 +1,21 @@
 class Oystercard
 
   attr_reader :balance
+  DEFAULT_MAX_BALANCE = 90
 
-  def initialize
+  def initialize(max_balance = DEFAULT_MAX_BALANCE)
+    @max_balance = max_balance
     @balance = 0
   end
 
   def top_up(amount)
-    @balance += amount
+    if (@balance + amount) > @max_balance
+      puts "Error: Top-up will exceed maximum balance of £#{@max_balance}"
+      false
+    else
+      @balance += amount
+      true
+    end
   end
 
 end
